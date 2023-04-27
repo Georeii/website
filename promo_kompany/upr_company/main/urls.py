05b71e57e88077ad076b91d35c01ee54d7718121
Georeii <109893926+Georeii@users.noreply.github.com>
2023-04-27 10:26:55 +0300

    чуть подправил имена
    надеюсь не сломается

--- promo_kompany/upr_company/main/urls.py
+++ promo_kompany/upr_company/main/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('personal_area',views.personal_area, name = 'Personal_area'),
+    path('personal_area',views.personal_area, name = 'personal_area'),
     path('registration',views.registration, name = 'registration'),
-    path('entrance', views.entrance, name = 'Entrance'),
-    path('companies',views.companies, name = 'Companies'),
-    path('People',views.People, name = 'People'),
+    path('entrance', views.entrance, name = 'entrance'),
+    path('companies',views.companies, name = 'companies'),
+    path('People',views.people, name = 'people'),
     path('create/',views.create,),
     path('entrance_pr', views.entrance_pr)
 
