92913ff7e17c6d4875bab261db540deff64df3b2
DESKTOP-66C0NUN\georg <gk211082@gmail.com>
2023-05-13 12:40:09 +0300

    vyjuj xtuj bynthtcyjuj

--- promo_kompany/upr_company/main/models.py
+++ promo_kompany/upr_company/main/models.py
@@ -1,32 +1,25 @@
 from django.db import models
-
-
-class People(models.Model):
-	Email = models.CharField(max_length=50)
-	password = models.CharField(max_length=32)
-	name = models.CharField(max_length=50)
-	surname = models.CharField(max_length=50)
-	phone_number = models.CharField(max_length=12)
+from django.contrib.auth.models import User
 
 
 class Company(models.Model):
-	people = models.ManyToManyField(People)
+	User = models.ManyToManyField(User)
 	name_company = models.CharField(max_length=50)
 
 
 class Home(models.Model):
-	company = models.ManyToManyField(Company)
+	Company = models.ManyToManyField("Company")
 	city = models.TextField()
 	street = models.TextField()
 	num_home = models.CharField(max_length=10)
-	entrase = models.FloatField()
+	col_apartment = models.FloatField()
 
 
 class Bypass_result(models.Model):
-	company = models.ForeignKey(Company, on_delete=models.CASCADE )
+	Company = models.ForeignKey(Company, on_delete=models.CASCADE )
 	home = models.ForeignKey(Home, on_delete=models.CASCADE )
 	apartment = models.FloatField()
 	open_close = models.FloatField()
 	reaction = models.FloatField()
 	# open_close 1 открыто 0 закрыто
-	# reaction 0 негативно, 1 нейтрально, 2 посыл в баню  
+	# reaction 0 негативно, 1 нейтрально, 2 положительно 
