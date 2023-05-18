c166de5241d65b749691a822b3d884b1bcfb1ffa
Georeii <109893926+Georeii@users.noreply.github.com>
2023-05-18 22:21:31 +0300

    Update settings.py

--- promo_kompany/upr_company/upr_company/settings.py
+++ promo_kompany/upr_company/upr_company/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pk+bd^0elx*+&7%ib*t#+b3a16=xm=i=u+jzvm@i0htmz=!53x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
